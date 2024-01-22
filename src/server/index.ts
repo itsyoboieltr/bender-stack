b92403278d7805bd223c795b00f6a2f5699db062
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2024-01-22 01:58:08 +0100

    fix: wrong exports

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,9 +5,3 @@ import { todoRoute } from './todo';
 export const app = new Elysia({ prefix: '/api' }).use(todoRoute).compile();
 
 export type App = typeof app;
-
-export const GET = app.handle;
-export const POST = app.handle;
-export const PUT = app.handle;
-export const PATCH = app.handle;
-export const DELETE = app.handle;
