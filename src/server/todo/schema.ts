76d34287a6ad56581ea6b89009313b9b808f1687
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2024-08-25 04:22:36 +0400

    feat: ulid

--- src/server/todo/schema.ts
+++ src/server/todo/schema.ts
@@ -1,10 +1,10 @@
-import { createId } from '@paralleldrive/cuid2';
 import { pgTable, text } from 'drizzle-orm/pg-core';
 import { createSelectSchema } from 'drizzle-typebox';
 import { t } from 'elysia';
+import { ulid } from 'ulidx';
 
 export const todo = pgTable('todo', {
-  id: text('id').primaryKey().$defaultFn(createId),
+  id: text('id').primaryKey().$defaultFn(ulid),
   data: text('data').notNull(),
 });
 
