5dd25a9af63791bbe755053be075af0828972f7c
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2024-11-13 02:14:01 +0400

    Update index.ts

--- utils/env/client/index.ts
+++ utils/env/client/index.ts
@@ -1,8 +1,11 @@
 import { z } from 'zod';
 
 export const clientEnv = z
-  .object({ EXPO_PUBLIC_HOST_URL: z.string().min(1).trim() })
-  .parse({
-    EXPO_PUBLIC_HOST_URL:
-      process.env.EXPO_PUBLIC_HOST_URL ?? 'http://localhost:3000',
-  });
+  .object({
+    EXPO_PUBLIC_HOST_URL: z
+      .string()
+      .min(1)
+      .trim()
+      .default('http://localhost:3000'),
+  })
+  .parse({ EXPO_PUBLIC_HOST_URL: process.env.EXPO_PUBLIC_HOST_URL });
