1021d36987a3e37e347839c49cc4c5b7fefe7591
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2024-11-14 02:06:29 +0400

    feat: sdk 52

--- index.ts
+++ index.ts
@@ -1,18 +1,8 @@
+import { createRequestHandler } from '@expo/server';
 import { name } from 'package.json';
 import path from 'path';
 
-const WebRequest = Request;
-const WebResponse = Response;
-
-const { createRequestHandler } = await import('@expo/server/build');
-
-// Expo overrides the global Request and Response, so we need to override them back
-global.Request = WebRequest;
-global.Response = WebResponse;
-
-const handleRequest = createRequestHandler(
-  path.join(import.meta.dir, 'dist/server')
-);
+const handler = createRequestHandler(path.join(import.meta.dir, 'dist/server'));
 
 const server = Bun.serve({
   async fetch(req) {
@@ -21,7 +11,7 @@ const server = Bun.serve({
       const file = Bun.file(`dist/client${url.pathname}`);
       return new Response(file);
     }
-    return await handleRequest(req);
+    return await handler(req);
   },
 });
 
