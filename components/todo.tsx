6447d63636d281661722dfb9b8b39ab0f484f667
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2024-12-24 18:08:49 +0100

    fix: deps

--- components/todo.tsx
+++ components/todo.tsx
@@ -10,11 +10,11 @@ export default function Todo(props: TodoProps) {
   const todoDelete = trpc.todo.delete.useMutation();
   const todoDeletingDisabled = todoDelete.isPending;
   return (
-    <View className={'flex flex-row justify-center items-center gap-4'}>
+    <View className={'flex flex-row items-center justify-center gap-4'}>
       <Text>{props.data}</Text>
       <Pressable
         className={cn(
-          'rounded border-2 border-black bg-red-300 transition-all px-4 py-1 flex flex-row items-center justify-center web:select-none',
+          'flex flex-row items-center justify-center rounded border-2 border-black bg-red-300 px-4 py-1 transition-all web:select-none',
           {
             'bg-red-400': todoDeletingDisabled,
             'hover:bg-red-400 active:bg-red-500': !todoDeletingDisabled,
