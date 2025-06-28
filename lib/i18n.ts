06000c112226a860b897e15c4689e46be65e9383
Norbert Elter <72046715+itsyoboieltr@users.noreply.github.com>
2025-06-28 22:04:55 +0400

    feat: set zod locale

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,21 +1,26 @@
 import { getLocales } from 'expo-localization';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { z } from 'zod/v4';
 
 import en from '~/assets/locales/en.json';
-
-import { storage } from './storage';
+import { storage } from '~/lib/storage';
 
 const resources = {
   en: { translation: en },
 };
 
-export const supportedLngs = Object.keys(resources);
+export type SupportedLanguage = keyof typeof resources;
+
+export const supportedLngs = Object.keys(resources) as [SupportedLanguage];
 
 const locales = getLocales();
 
-const firstDeviceLocale = locales[0]?.languageCode;
-const fallbackLocale = 'en';
+const firstDeviceLocale = locales[0]?.languageCode as
+  | SupportedLanguage
+  | undefined;
+
+const fallbackLocale = 'en' satisfies SupportedLanguage;
 
 const defaultLanguage = firstDeviceLocale
   ? supportedLngs.includes(firstDeviceLocale)
@@ -23,10 +28,17 @@ const defaultLanguage = firstDeviceLocale
     : fallbackLocale
   : fallbackLocale;
 
-const lng =
-  typeof window !== 'undefined'
-    ? (storage.get('language') ?? defaultLanguage)
-    : defaultLanguage;
+const storedLanguage = (
+  typeof window !== 'undefined' ? storage.get('language') : null
+) as SupportedLanguage | null;
+
+const lng = storedLanguage
+  ? supportedLngs.includes(storedLanguage)
+    ? storedLanguage
+    : defaultLanguage
+  : defaultLanguage;
+
+z.config(z.locales[lng]());
 
 export default i18n.use(initReactI18next).init({
   supportedLngs,
